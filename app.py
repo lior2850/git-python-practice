1630dddc5f9dd128118eaa79b9916b6cfbc9d1ba
lior2850 <lior2850@gmail.com>
2026-01-17 18:51:51 +0200

    adding current time to the app

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+
 def greet(name):
-    return f"Hello, {name}!"
+    current_time = datetime.now().strftime("%H:%M:%S")
+    return f"Hello, {name}! The current time is {current_time}."
 
 if __name__ == "__main__":
-     user = "World"
-     print(greet(user))
-                
+    user = "World"
+    print(greet(user))
